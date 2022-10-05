78b1cd01707380b416bcd94a5307b8058b86bcc9
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-10-05 14:36:08 +0200

    add funtionality for editing product, created another Context API

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,26 @@ import {Main} from './components/Main/Main';
 import {GlobalContext} from './contexts/global.context';
 import {SearchContext} from './contexts/search.context';
 import {OfferContext} from "./contexts/offer.context";
+import {IdContext} from './contexts/id.context';
 
 
 export const App = () => {
     const [search, setSearch] = useState('');
     const [searchOffer, setSearchOffer] = useState('');
+    const [id, setId] = useState('');
     const [randomNumber, setRandomNumber] = useState(0);
 
     return (
         <OfferContext.Provider value={{searchOffer, setSearchOffer}}>
             <GlobalContext.Provider value={{randomNumber, setRandomNumber}}>
-                <SearchContext.Provider value={{search, setSearch}}>
-                    <>
-                        <Header/>
-                        <Main/>
-                    </>
-                </SearchContext.Provider>
+                <IdContext.Provider value={{id, setId}}>
+                    <SearchContext.Provider value={{search, setSearch}}>
+                        <>
+                            <Header/>
+                            <Main/>
+                        </>
+                    </SearchContext.Provider>
+                </IdContext.Provider>
             </GlobalContext.Provider>
         </OfferContext.Provider>
     )
--- src/components/Main/aside/OptionsView.tsx
+++ src/components/Main/aside/OptionsView.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import {ChangePrices} from "./ChangePrices/ChangePrices";
 import {ShowOfferDetails} from "./ShowOfferDetails/ShowOfferDetails";
 import {AddNewProduct} from "./AddNewProduct/AddNewProduct";
+import { EditProduct } from "./EditProduct/EditProduct";
 
 export const OptionsView = () => {
     return (
@@ -10,6 +11,7 @@ export const OptionsView = () => {
             <Route path="/change-prices" element={<ChangePrices/>}/>
             <Route path="/show-offer" element={<ShowOfferDetails/>}/>
             <Route path="/add-new-product" element={<AddNewProduct/>}/>
+            <Route path="/edit-product" element={<EditProduct/>}/>
         </Routes>
     )
 };
